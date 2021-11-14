55fd517bc84c8b41984e4ccc99b9029c0e98fe18
Арсений Васенда <1801200095@office.vsu.by>
2021-11-15 01:06:15 +0300

    <Required>

---
+++ Pract6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pract6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
