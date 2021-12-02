95dba101eb9a1cd8a8cc2439e0cb02d524f66fd8
Арсений Васенда <1801200095@office.vsu.by>
2021-12-02 03:17:34 +0300

    Added logging and exception handling

---
+++ Pract6/CustomExceptions/OutOutStockException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract6
+{
+    public class OutOutStockException : Exception
+    {
+        public OutOutStockException()
+        {
+        }
+        public OutOutStockException(string message) 
+            : base(message)
+        {
+        }
+        public OutOutStockException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
--- Pract6/Rootobject.cs
+++ Pract6/Rootobject.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Pract6
 {
     public class RootObject
     {
         public Shop[] Shops { get; set; }
+        private int countP;
+        private int countS;
+        private static ILoggerFactory loggerFactory = LoggerFactory.Create(config =>
+        {
+            config.AddConsole();
+        });
+        private ILogger<RootObject> logger = loggerFactory.CreateLogger<RootObject>();
 
         public void DispalyIfAvaliable()
         {
@@ -16,7 +24,7 @@ namespace Pract6
             int androidCount = 0;
             for (int i = 0; i < Shops.Length; i++)
             {
-                Console.WriteLine($"[Id] [Name]\n{Shops[i].Id} {Shops[i].Name}\n[Description]\n{Shops[i].Description}\n[Amount of phones in stock]");
+                logger.LogInformation($"[Id] [Name]\n      {Shops[i].Id} {Shops[i].Name}\n      [Description]\n      {Shops[i].Description}\n      [Amount of phones in stock]");
                 for (int j = 0; j < Shops[i].Phones.Length; j++)
                 {
                     if (Shops[i].Phones[j].OperationSystemType == "IOS")
@@ -28,73 +36,109 @@ namespace Pract6
                         androidCount++;
                     }
                 }
-                Console.WriteLine($"{iosCount} IOS based phones are avaliable");
-                Console.WriteLine($"{androidCount} Android based phones are avaliable");
+                logger.LogInformation($"{iosCount} IOS based phones are avaliable");
+                logger.LogInformation($"{androidCount} Android based phones are avaliable");
                 iosCount = 0;
                 androidCount = 0;
             }
         }
 
-        public void WantToBuy()
+        public void WantToBuyPhone()
         {
-            Console.WriteLine("Which mobile phone do you want to buy?");
-            string model;
-            string shop;
-            model = Console.ReadLine();
+            string phoneModel;
             List<Phone> purchase = new List<Phone>();
-            bool isFound = false;
+            logger.LogWarning("Which mobile phone do you want to buy?");
+            phoneModel = Console.ReadLine();
             for (int i = 0; i < Shops.Length; i++)
             {
                 for (int j = 0; j < Shops[i].Phones.Length; j++)
                 {
-                    if(model == Shops[i].Phones[j].Model)
+                    if(phoneModel == Shops[i].Phones[j].Model)
                     {
-                        if(Shops[i].Phones[j].IsAvailable == true)
+                        try
                         {
-                            purchase.Add(Shops[i].Phones[j]);
+                            if (Shops[i].Phones[j].IsAvailable == true)
+                            {
+                                purchase.Add(Shops[i].Phones[j]);
+                            }
+                            else
+                            {
+                                countP++;
+                                if (countP < 2)
+                                {
+                                    logger.LogInformation("This mobile phone is out of stock. Choose another model.");
+                                    WantToBuyPhone();
+                                }
+                                else
+                                {
+                                    throw new OutOutStockException();
+                                }
+                            }
                         }
-                        else
+                        catch(OutOutStockException)
                         {
-                            Console.WriteLine("This mobile phone is out of stock");
-                            WantToBuy();
+                            logger.LogError("This mobile phone is out of stock.");
                         }
                     } 
                 }
             }
-            if(purchase.Count == 0)
+            if(purchase.Count == 0 && countP < 2)
             {
-                Console.WriteLine("This mobile phone is not found");
-                WantToBuy();
+                logger.LogInformation("This mobile phone is not found");
+                WantToBuyPhone();
             }
             if(purchase.Count > 0)
             {
-                Console.WriteLine("Your request is satisfied.");
-                Console.WriteLine($"{purchase[0].Model}\n{purchase[0].OperationSystemType}\n{purchase[0].MarketLaunchDate}\n{purchase[0].Price}");
+                logger.LogInformation("Your request is satisfied.");
+                logger.LogInformation($"[Model]\n      {purchase[0].Model}\n      [Operating system]\n      {purchase[0].OperationSystemType}\n      [Market Launch]\n      {purchase[0].MarketLaunchDate}\n      [Price]\n      ${purchase[0].Price}");
                 if (purchase.Count == 1)
                 {
-                    Console.WriteLine($"You can purchase it on {Shops[purchase[0].ShopId - 1].Name}\n{Shops[purchase[0].ShopId - 1].Description}");
+                    logger.LogInformation($"You can purchase it in the {Shops[purchase[0].ShopId - 1].Name}\n      {Shops[purchase[0].ShopId - 1].Description}");
+                    WantToBuyShop(purchase);
                 }
                 else if(purchase.Count > 1)
                 {
-                    Console.WriteLine("You can purchase it in the following shops:");
+                    logger.LogInformation("You can purchase it in the following shops:");
                     for(int i = 0; i <= purchase.Count -1; i++)
                     {
-                        Console.WriteLine($"{Shops[purchase[i].ShopId - 1].Name}\n{Shops[purchase[i].ShopId - 1].Description}");
+                        logger.LogInformation($"{Shops[purchase[i].ShopId - 1].Name}\n      {Shops[purchase[i].ShopId - 1].Description}");
                     }
+                    WantToBuyShop(purchase);
                 }
-                Console.WriteLine($"In which store do you want to buy the mobile phone {purchase[0].Model}?");
-                shop = Console.ReadLine();
-                for (int i = 0; i <= purchase.Count - 1; i++)
+            }
+        }
+        private void WantToBuyShop(List<Phone> purchase)
+        {
+            string shop;
+            bool isFound = false;
+            logger.LogWarning($"In which store do you want to buy the mobile phone {purchase[0].Model}?");
+            shop = Console.ReadLine();
+            for (int i = 0; i <= purchase.Count - 1; i++)
+            {
+                if (shop == Shops[purchase[i].ShopId - 1].Name)
                 {
-                    if (shop == Shops[purchase[i].ShopId - 1].Name)
+                    logger.LogInformation($"Order for {purchase[i].Model} ({purchase[i].OperationSystemType}), price ${purchase[i].Price}, market launch date {purchase[i].MarketLaunchDate}, in shop {Shops[purchase[i].ShopId - 1].Name} has been successfully placed.");
+                    isFound = true;
+                }
+            }
+            if (isFound == false)
+            {
+                try
+                {
+                    countS++;
+                    if (countS < 2)
                     {
-                        Console.WriteLine($"Order for {purchase[i].Model} ({purchase[i].OperationSystemType}), price ${purchase[i].Price}, market launch date {purchase[i].MarketLaunchDate}, in shop {Shops[purchase[i].ShopId - 1].Name} has been successfully placed.");
-                        isFound = true;
+                        logger.LogInformation("This shop is not found. Choose another shop.");
+                        WantToBuyShop(purchase);
+                    }
+                    else
+                    {
+                        throw new NotFoundException();
                     }
                 }
-                if(isFound == false)
+                catch(NotFoundException)
                 {
-                    Console.WriteLine("This shop is not found");
+                    logger.LogError("This shop is not found.");
                 }
             }
         }
