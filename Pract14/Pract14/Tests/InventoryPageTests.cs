d8c479ed78344abdc850b85070b1157097b07796
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-08 18:18:10 +0300

    Create InventoryPageTests.cs

---
+++ Pract14/Pract14/Tests/InventoryPageTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Pract14
+{
+    [TestFixture]
+    public class InventoryPageTests : BaseTest
+    {
+        [Test]
+        [TestCase("hilo")]
+        [TestCase("lohi")]
+        public void testtest(string sortWay)
+        {
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.LogInAs(Users.StandardUser.Username, Configurator.Password);
+            InventoryPage page = new InventoryPage(Driver);
+            page.DropDown_ProductSort.SelectByValue(sortWay);
+            var actualSort = page.GetPricesList();
+            if (sortWay == "hilo")
+            {
+                var expectedSort = actualSort.OrderByDescending(s => s);
+                Assert.IsTrue(expectedSort.SequenceEqual(actualSort));
+            }
+            else if (sortWay == "lohi")
+            {
+                var expectedSort = actualSort.OrderBy(s => s);
+                Assert.IsTrue(expectedSort.SequenceEqual(actualSort));
+            }
+        }
+    }
+}
