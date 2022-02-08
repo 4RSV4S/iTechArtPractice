cee64436cd643655a0e5f85af5fefa8edbf50531
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-08 17:45:44 +0300

    Create InventoryPage.cs

---
+++ Pract14/Pract14/Pages/InventoryPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+
+namespace Pract14
+{
+    public class InventoryPage : BasePage
+    {
+        private static readonly By Link_ShoppingCartBy = By.Id("shopping_cart_container");
+        private static readonly By DropDown_ProductSortBy = By.ClassName("product_sort_container");
+        private static readonly By Div_InventoryItemPriceBy = By.ClassName(".inventory_item_price");
+
+        public InventoryPage(IWebDriver driver) : base(driver)
+        {
+        }
+        protected override void OpenPage()
+        {
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Driver.Navigate().GoToUrl(Configurator.BaseURL + Configurator.InventoryPage_END_POINT);
+        }
+
+        public List<double> GetPricesList()
+        {
+            var priceList = new List<double>();
+            foreach (var p in InventoryItemPrices)
+            {
+                priceList.Add(Convert.ToDouble(p.Text.Replace('$',' ')));
+            }
+            return priceList;
+        }
+
+        public IWebElement Link_ShoppingCart => Driver.FindElement(Link_ShoppingCartBy);
+        public SelectElement DropDown_ProductSort =>  new SelectElement(Driver.FindElement(DropDown_ProductSortBy));
+        public IEnumerable<IWebElement> InventoryItemPrices => Driver.FindElements(Div_InventoryItemPriceBy);
+        
+
+    }
+}
