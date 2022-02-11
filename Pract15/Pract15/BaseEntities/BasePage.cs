9f16ee9a75dac42519970b56d7736eed01c8b67d
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-11 16:00:08 +0300

    Initial commit

---
+++ Pract15/Pract15/BaseEntities/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Pract15
+{
+    public abstract class BasePage
+    {
+        protected string BaseURL = "https://catalog.onliner.by/";
+        protected static IWebDriver Driver;
+
+        protected abstract void OpenPage();
+
+        public BasePage(IWebDriver Driver)
+        {
+            Driver = Driver;
+            OpenPage();
+        }
+    }
+}
---
+++ Pract15/Pract15/Pages/TVPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Pract15
+{
+    public class TVPage : BasePage
+    {
+        private static readonly By CheckBox_AddToComparison = By.XPath("//*[@title='В сравнение' and not(ancestor::div[@class='schema-product schema-product_children'])]//*[@type='checkbox']");
+        private string END_POINT = "tv";
+
+        public TVPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        protected override void OpenPage()
+        {
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Driver.Navigate().GoToUrl(BaseURL + END_POINT);
+        }
+
+        public IEnumerable<IWebElement> CheckBoxList => Driver.FindElements(CheckBox_AddToComparison);
+    }
+}
