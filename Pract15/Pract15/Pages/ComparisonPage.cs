cc00118ffbe9d7c2007bf2c889b97026e6a96f67
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-15 13:52:56 +0300

    Create ComparisonPage.cs

---
+++ Pract15/Pract15/Pages/ComparisonPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Pract15
+{
+    public class ComparisonPage : BasePage
+    {
+        private static readonly By Button_CompareBy = By.XPath("//a[contains(@href, 'compare')]");
+        public ComparisonPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public IWebElement Button_Compare = Driver.FindElement(Button_CompareBy);
+    }
+}
