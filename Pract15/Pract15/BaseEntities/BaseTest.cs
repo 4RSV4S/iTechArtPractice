d999090641cac4bf662f0687fb2e95ea0d1e020a
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-15 13:52:12 +0300

    Update BaseTest.cs

---
+++ Pract15/Pract15/BaseEntities/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Pract15
+{
+    public class BaseTest
+    {
+        protected static IWebDriver Driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
