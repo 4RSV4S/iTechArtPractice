08a24b2967c09eec2a2a16e71bee66da4b46f3ef
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-15 13:54:23 +0300

    Create TVPageTest.cs

---
+++ Pract15/Pract15/Tests/TVPageTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Pract15.Tests
+{
+    [TestFixture]
+    public class TVPageTest : BaseTest
+    {
+        [Test]
+        public void Scenario1()
+        {
+            TVPage tvPage = new TVPage(Driver);
+
+            tvPage.CheckBoxList[0].Click();
+            Assert.AreEqual(tvPage.CheckBoxList[0].GetAttribute("title"), "В сравнении");
+            tvPage.CheckBoxList[1].Click();
+            Assert.AreEqual(tvPage.CheckBoxList[1].GetAttribute("title"), "В сравнении");
+
+            ComparisonPage comparisonPage = new ComparisonPage(Driver);
+            comparisonPage.Button_Compare.Click();
+        }
+    }
+}
