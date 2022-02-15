b1a351d049da8da97bccdfcc42a8ffec862a62c0
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-15 13:51:08 +0300

    Update TVPage.cs

--- Pract15/Pract15/Pages/TVPage.cs
+++ Pract15/Pract15/Pages/TVPage.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 using OpenQA.Selenium;
+using System.Linq;
 
 namespace Pract15
 {
     public class TVPage : BasePage
     {
-        private static readonly By CheckBox_AddToComparison = By.XPath("//*[@title='В сравнение' and not(ancestor::div[@class='schema-product schema-product_children'])]//*[@type='checkbox']");
+        private static readonly By CheckBox_AddToComparisonBy = By.XPath("//*[contains(@title,'В сравнени') and not(ancestor::div[@class='schema-product schema-product_children'])]");
         private string END_POINT = "tv";
 
         public TVPage(IWebDriver driver) : base(driver)
         {
+            OpenPage();
         }
 
-        protected override void OpenPage()
+        public void OpenPage()
         {
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             Driver.Navigate().GoToUrl(BaseURL + END_POINT);
         }
 
-        public IEnumerable<IWebElement> CheckBoxList => Driver.FindElements(CheckBox_AddToComparison);
+        public IList<IWebElement> CheckBoxList => Driver.FindElements(CheckBox_AddToComparisonBy);
     }
 }
